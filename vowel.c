9dc5117ff2a318e40be16f8c55830ae0c27569e5
ilakkiyaisaiya <31511905+ilakkiyaisaiya@users.noreply.github.com>
2018-01-09 19:35:21 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char a;
+scanf("%c",&a);
+if((a=='a')||(a=='A')||(a=='e')||(a=='E')||(a=='i')||(a=='I')||(a=='o')||(a=='O')||(a=='u')||(a=='U'))
+{
+printf("vowel");
+}
+else 
+{
+printf("consonant");
+}
+}
