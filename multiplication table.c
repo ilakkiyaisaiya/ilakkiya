5f42ebfb20b669311bf9aac931767510ee89854b
ilakkiyaisaiya <31511905+ilakkiyaisaiya@users.noreply.github.com>
2018-01-19 16:38:01 +0530

    Create multiplication table.c

---
+++ multiplication table.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int n,limit,m=o;
+printf("table no and limit");
+scanf("%d%d",&n,&limit);
+printf("multiplication table);
+for(inti=1;i<=limit;i++)
+{
+m=i*n;
+printf("%d%d=%d",i,n,.m);
+}
+}
+
