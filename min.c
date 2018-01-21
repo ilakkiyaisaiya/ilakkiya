2138e4f5e2d542059e21a5af5af4a5c7c4711f90
ilakkiyaisaiya <31511905+ilakkiyaisaiya@users.noreply.github.com>
2018-01-21 04:33:18 +0000

    Create min.c

---
+++ min.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void main() 
+    {    
+    int array[100], minimum, size, c, l= 1;
+    
+    printf("Enter the number of elements in array\n");
+    scanf("%d",&size);
+    
+    printf("Enter %d integers\n", size);
+    
+    for ( c = 0 ; c < size ; c++ )
+    scanf("%d", &array[c]);    
+    
+    minimum = array[0];
+    
+    for ( c = 1 ; c < size ; c++ ) 
+    {
+        if ( array[c] < minimum ) 
+        {
+minimum = array[c];
+l= c+1;
+        }
+    } 
+    
+    printf("Minimum element is present at l%d and it's value is %d.\n", l, minimum);
+      }
+    
