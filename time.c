e489f8cbefc7e5d123899e2d6f2d4d6b7dcc8f32
ilakkiyaisaiya <31511905+ilakkiyaisaiya@users.noreply.github.com>
2018-01-21 10:28:09 +0530

    Create time.c

---
+++ time.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+  int mins,hours,time;
+printf("time in minutes is");
+scanf("%d",&min);
+time=min/60;
+printf("time in hours is %d hours",time);
+getch();
+return 0;
+}
