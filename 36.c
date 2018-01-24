59749d9ebb078e81a665d1d145df2b74888b40fe
ilakkiyaisaiya <31511905+ilakkiyaisaiya@users.noreply.github.com>
2018-01-24 16:10:27 +0530

    Create 36.c

---
+++ 36.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    char string[80];
+    int count, nc = 0, sum = 0;
+    scanf("%s", string);
+    for (count = 0; string[count] != '\0'; count++)
+    {
+        if ((string[count] >= '0') && (string[count] <= '9'))
+        {
+            nc =nc+1;
+            sum =sum+ (string[count] - '0');
+        }
+    }
+    printf("NO. of special characters in the string = %d\n", nc);
+    printf("Sum of all digits = %d\n", sum);
+    return 0;
+}
+
