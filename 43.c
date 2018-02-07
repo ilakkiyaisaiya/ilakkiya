52e33a0135d1dfc4b3ec0ec6f263db3220369274
ilakkiyaisaiya <31511905+ilakkiyaisaiya@users.noreply.github.com>
2018-02-07 15:52:06 +0530

    Create 43.c

---
+++ 43.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	char a[10];	char b[10];
+	printf("enter string 1\n");
+    scanf(#"%s",&a);
+	printf("enter string 2\n");
+	scanf("%s",&b);
+	printf("concatenated strings:%s%s",a,b);
+}
